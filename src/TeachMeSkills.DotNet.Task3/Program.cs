99a2701ce19a9695260122f575e838595ea141be
SergeyBernat <vinex99@gmail.com>
2020-09-18 21:29:44 +0300

    feat: add solution of 3 task

---
+++ src/TeachMeSkills.DotNet.Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double r = double.Parse(Console.ReadLine());
+            var S = pi * r * r;
+            Console.WriteLine(S);
+
+
+        }
+    }
+}
