0fcae52ba8aa5a63b5e196ef766441a370e2dc38
SergeyBernat <vinex99@gmail.com>
2020-09-18 21:19:58 +0300

    feat: add solution of 2 task

---
+++ src/TeachMeSkills.DotNet.Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "\nмоя строка 1";
+            string b = "\tмоя строка 2";
+            string c = "\aмоя строка 3";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
