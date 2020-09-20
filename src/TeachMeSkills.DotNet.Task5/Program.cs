0c90b07f565ae33608b95bade0d571aa0f90791b
SergeyBernat <vinex99@gmail.com>
2020-09-20 18:28:37 +0300

    feat: add solution of 5 task

---
+++ src/TeachMeSkills.DotNet.Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 3;
+            Console.WriteLine($"{num1+num2}, {num1-num2}, {num1/num2}, {num1*num2}, {num1%num2}, {++num1}, {--num2}");
+        }
+    }
+}
