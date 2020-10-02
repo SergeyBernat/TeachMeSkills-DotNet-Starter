fa5330222e7fd74b5b3dd56b49af7fd2515b1e61
SergeyBernat <vinex99@gmail.com>
2020-10-02 16:24:28 +0300

    feat: add solution of 27 task

---
+++ src/TeachMeSkills.DotNet.Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massive = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Array.Reverse(massive);
+            foreach (int i in massive)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
