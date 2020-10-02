22c0299e14007d2ec964f8847c8bc40dc125603c
SergeyBernat <vinex99@gmail.com>
2020-10-02 15:22:09 +0300

    feat: add solution of 16 task

---
+++ src/TeachMeSkills.DotNet.Task16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter leght:");
+            int length = Convert.ToInt32(Console.ReadLine());
+            if (width != length)
+            {
+                for (int i = 1; i < width + 1; i++)
+                {
+                    for (int a = 1; a < length; ++a)
+                    {
+                        Console.Write("*");
+                        Console.Write(" ");
+                    }
+                    Console.Write("*");
+                    Console.Write("\n");
+                }
+    }
+}
