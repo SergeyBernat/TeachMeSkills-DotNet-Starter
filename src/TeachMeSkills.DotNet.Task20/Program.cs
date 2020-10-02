8c973728bcad2e1f1c5309d42b3ddabac69babb3
SergeyBernat <vinex99@gmail.com>
2020-10-02 15:39:08 +0300

    feat: add solution of 20 task

---
+++ src/TeachMeSkills.DotNet.Task20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            var num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            var num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number:");
+            var num3 = double.Parse(Console.ReadLine());
+            double result = Calculate(num1, num2, num3);
+            Console.WriteLine(result);
+        }
+
+        private static double Calculate(double num1, double num2, double num3)
+        {
+            return (num1 + num2 + num3) / 3;
+        }
+    }
+}
