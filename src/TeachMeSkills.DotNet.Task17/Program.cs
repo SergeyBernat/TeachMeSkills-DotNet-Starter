bcdae9d41bd935864491423de0a31f0da246323e
SergeyBernat <vinex99@gmail.com>
2020-10-02 15:13:44 +0300

    feat: add solution of 17 task

---
+++ src/TeachMeSkills.DotNet.Task17/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter quantity RUB:");
+            var Rubl = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter conversion rade of RUB to USD: ");
+            decimal Conv = decimal.Parse(Console.ReadLine());
+            decimal Doll = Rubl / Conv;
+            Console.WriteLine($"{Rubl} RUB is {Doll} USD");
+        }
+    }
+}
